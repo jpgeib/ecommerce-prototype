5d7a859cad0f714c2f1bf0390e65b28fb5d26818
James Geib <jpgeib2@gmail.com>
2022-07-27 10:54:14 -0400

    moved App files into containers and renamed them

---
+++ client/src/containers/App/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import './style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        
+      </>
+    );
+  }
+}
+
+export default App;
