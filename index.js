31f1f6ab3e3c92ae4731b51a10637e960fc307f8
James Geib <jpgeib2@gmail.com>
2022-07-27 11:54:17 -0400

    created folder trees for initial apiRoutes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //Boilerplate
 const express = require("express");
+const routes = require("./routes");
+
 const app = express();
 const server = require("http").createServer(app);
 const path = require("path");
@@ -9,6 +11,7 @@ const PORT = process.env.PORT || 3001;
 //Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 if(process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
