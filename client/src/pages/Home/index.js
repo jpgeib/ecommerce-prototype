99817e2da55fc7d8e3b5394f8b01f68ad34ebda7
James Geib <jpgeib2@gmail.com>
2022-07-27 11:04:04 -0400

    created initial Home page

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Grid, Header } from "semantic-ui-react";
+
+class Home extends Component {
+    render() {
+        return (
+            <Grid>
+                <Header as="h1">Home</Header>
+            </Grid>
+        );
+    }
+}
